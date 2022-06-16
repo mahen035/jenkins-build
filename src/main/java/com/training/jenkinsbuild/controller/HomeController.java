81bcb0b4cb97b330887ddb5f0b0faf1cd158f022
amdcdac01 <amdcdac01@amdcdac01.iiht.tech>
2022-06-16 07:03:31 +0530

    initial commit

---
+++ src/main/java/com/training/jenkinsbuild/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.training.jenkinsbuild.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("home")
+public class HomeController {
+	
+	@GetMapping("/msg")
+	public String getMsg() {
+		return "Deployed successfully by Jenkins";
+	}
+
+}
